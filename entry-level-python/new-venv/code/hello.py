525ebeb5c2c790688c1bff4f2831f6bf61115eb2
Mario Alvarez <mario.b.alvarez@accenture.com>
2023-04-25 10:05:52 -0600

    setting up python versions

---
+++ entry-level-python/new-venv/code/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python310
+#Having differebnt verisons of python. Intalls all versions you want (Addignn to env variables)
+#thge firts one that apear in env variables is gpoing to take the python keyword
+# the other ones i have to rename the execuatble with the version python.exe and pythonw.exe to python310.exe and pythonw310.exe
+#Modify the env variavble with tishi new names
+#So here i  can select with interpreter i want, especifaying  env python310 or env python(3.11) 
+import sys
+print("Hello, World")
+print(sys.version)
