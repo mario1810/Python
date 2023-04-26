d5d0c0ac472bb8d3180d6054a585441c2fc6642a
Mario Alvarez <mario.b.alvarez@accenture.com>
2023-04-26 10:17:01 -0600

    second lab

---
+++ entry-level-python/new-venv/code/string-info.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+message=input("Enter a message: ")
+print("First character: "+message[0])
+print("Last character: "+ message[-1])
+print("Middle charater: "+message[len(message)//2])
+print("Even index characters: " +message[::2])
+print("Odd index characters: "+ message[1::2])
+print("Reversed message: "+ message[::-1])
